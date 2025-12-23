b62e38e19c48a81f0aa6a6f98c201bb7a9781937
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2025-12-23 21:57:35 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  );
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
